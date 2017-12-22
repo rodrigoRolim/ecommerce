fa6ff3ce1479f86d3998617f44ca7df5d44148d1
Rodrigo Rolim <rodriggit config --global user.name Rodrigo>
2017-12-22 16:30:23 -0200

    Categories vs Products

--- site.php
+++ site.php
@@ -14,19 +14,11 @@ $app->get("/categories/:idcategory", function($idcategory) {
 	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 	$category = new Category();
 	$category->get((int)$idcategory);
-	$pagination = $category->getProductsPage($page);
-	$pages = [];
-	for ($i=1; $i <= $pagination['pages']; $i++) { 
-		array_push($pages, [
-			"link"=>'/sites/trunk/danillo10/e-commerce/categories/'.$category->getidcategory().'?page='.$i,
-			"page"=>$i
-		]);
-	}
+
 	$page = new Page();
 	$page->setTpl("category", array(
 		"category"=>$category->getValues(),
-		"products"=>$pagination["data"],
-		"pages"=>$pages
+		"products"=>Product::checkList($category->getProducts())
 	));
 });
 $app->get("/products/:desurl", function($desurl) {
@@ -42,4 +34,5 @@ $app->get("/cart", function(){
 	$page = new Page();
 	$page = setTpl("cart");
 });
+
  ?>
